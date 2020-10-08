21dc9e8e875f5b328e3543ca70d66ea3f0cd07e2
mateopizzolo <mpizzolo@udesa.edu.ar>
2020-10-07 22:30:35 -0300

    b

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
+import Img from './components/Img';
+import LinkReact from './components/LinkReact.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">       
+
+      <Img/>
+        <Title/>
+        <LinkReact/>
+        
       </header>
     </div>
   );
---
+++ src/components/Img.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '.././logo.svg'
+
+function Img() {
+    return (
+        <img src={logo} className="App-logo" alt="logo" />
+    )
+};
+
+export default Img;
